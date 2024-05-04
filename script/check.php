a1848b5ae20349f1b8fcca08f364d2001e4358df
bunsaijux <128596786+bunsaijux@users.noreply.github.com>
2024-05-05 01:32:07 +0700

    Update check.php

--- script/check.php
+++ script/check.php
@@ -15,11 +15,11 @@ $hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 $fh = fopen('logs.txt', 'a'); 
 fwrite($fh, ''."".$ip ."\n");
 $keys = array(
+   
 "NIMO-351896329-SHOP",
-
 "NIMO-35134896329-SHOP",
-
 "NIMO-35189653244529-SHOP"
+   
 ); 
 $sub = $_GET["key"];
 if (in_array($sub,$keys,TRUE)) {
